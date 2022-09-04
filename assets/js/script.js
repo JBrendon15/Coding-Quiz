cbddc368fb3e023cd0955704e1f043fe83ab6d63
JBrendon15 <110281109+JBrendon15@users.noreply.github.com>
2022-09-04 03:44:45 -0500

    added some styling to css file and defined some variables on script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
-console.log("hello");
+let startBtn = document.getElementById('start');
+let currentQuestion = document.getElementById('current-question');
+let currentOptions = document.getElementById('options');
+
+startBtn.addEventListener('click', function(){
+    // console.log('hello');
+    
+})
