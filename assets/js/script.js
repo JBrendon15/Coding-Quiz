4f1c3305096ad04de5045b5a97d6e39de14d3860
JBrendon15 <110281109+JBrendon15@users.noreply.github.com>
2022-09-05 03:54:28 -0500

    added some css styling and functions to script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,37 @@
 let startBtn = document.getElementById('start');
-let currentQuestion = document.getElementById('current-question');
-let currentOptions = document.getElementById('options');
+let greeting = document.getElementById('start-page')
+let shownQuestion = document.getElementById('current-question');
+let questionsPage = document.getElementById('questions');
+let shownOptions = document.getElementById('options');
+let questionIndex = 0;
+let timeLeft = questions.length * 15;
+let timeShown = document.getElementById('time-left');
 
-startBtn.addEventListener('click', function(){
-    // console.log('hello');
+function timer(){
+    timeShown.textContent = timeLeft;
+    timeLeft--;
+}
+function startQuiz() {
+    greeting.setAttribute('class', 'hidden');
+    questionsPage.removeAttribute('class');
+    let startTime = setInterval(timer, 1000);
+    startQuestions();
+}
+function startQuestions(){
+    let currentQuestion = (questions[questionIndex]);
+    shownQuestion.textContent = currentQuestion.title;
+    for (let i = 0; i < currentQuestion.options.length; i++){
+        let option = currentQuestion.options[i];
+        let optionButton = document.createElement('button');
+        optionButton.textContent = i + 1 + '. ' + option;
+        shownOptions.appendChild(optionButton);
+    }
+}
+function
+
     
-})
+    
+
+
+
+startBtn.addEventListener('click', startQuiz);
